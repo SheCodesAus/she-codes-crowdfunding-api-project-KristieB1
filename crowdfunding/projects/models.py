87d1e357406fe24955a75d420e15e0c98d5bc446
Kristie <kmlbrandis@gmail.com>
2022-03-15 19:31:38 +0800

    added pledges endpoints

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,4 +1,4 @@
-from turtle import title
+# from turtle import title
 from django.db import models
 
 # Create your models here.
@@ -10,3 +10,15 @@ class Project(models.Model):
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
     owner = models.CharField(max_length=200)
+
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.CharField(max_length=200)
